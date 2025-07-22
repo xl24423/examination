55d544cbba9344fd940c53a276c834d654bfbad7
熊镭 <2442356950@qq.com>
2025-07-22 17:41:00 +0800

    版本内网穿透上线 1.0

--- src/main/java/cn/xl/examination/constBag/OssConst.java
+++ src/main/java/cn/xl/examination/constBag/OssConst.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class OssConst {
 
-    public static String accessKeyId;
-    public static String secret;
-
-
+    public static String accessKeyId = System.getenv("OSS_ACCESS_KEY_ID");;
+    public static String secret = System.getenv("OSS_ACCESS_KEY_SECRET");;
 
     public static final String bucketName = "xionglei-bucket";
     public static final String regionId = "cn-beijing";
