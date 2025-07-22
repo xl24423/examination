d20d840f709946e6b398e10978979e48f698baf5
熊镭 <2442356950@qq.com>
2025-07-22 17:21:21 +0800

    版本内网穿透上线 1.0

--- src/main/java/cn/xl/examination/constBag/OssConst.java
+++ src/main/java/cn/xl/examination/constBag/OssConst.java
@@ -1,8 +1,21 @@
 package cn.xl.examination.constBag;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class OssConst {
-    public static final String accessKeyId = "LTAI5t6kLL5LBS9XfsATMKZj";
-    public static final String secret = "CrQHZYnhCJUG65xGvF8gIZ5zKJ1rLd";
+
+    public static String accessKeyId;
+    public static String secret = "";
+    @Value("${aliyun.oss.access-key-id}")
+    public void setAccessKeyId(String accessKeyId){
+        OssConst.accessKeyId = accessKeyId;
+    }
+    @Value("${aliyun.oss.access-key-secret}")
+    public void setSecret(String secret){
+        OssConst.secret = secret;
+    }
+
+
     public static final String bucketName = "xionglei-bucket";
     public static final String regionId = "cn-beijing";
     public static final String roleArn = "acs:ram::1927946583376631:role/xiongleioss";
